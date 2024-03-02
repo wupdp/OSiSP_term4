31741c6a5dd612f4991b52578f1e62d9e730d3bf
wupdp <kirillbond2017@gmail.com>
2024-03-02 11:55:28 +0300

    ADD SORT "-s"

--- main.c
+++ main.c
@@ -8,10 +8,16 @@
 
 #define PATH_MAX 4096
 
+int compare_strings(const void *a, const void *b) {
+    return strcoll(*(const char **)a, *(const char **)b);
+}
+
 void dirwalk(const char *path, const char *options) {
     DIR *dir;
     struct dirent *entry;
     struct stat statbuf;
+    char **files = NULL;
+    int files_count = 0;
 
     if ((dir = opendir(path)) == NULL) {
         perror("opendir");
@@ -30,6 +36,7 @@ void dirwalk(const char *path, const char *options) {
         if (S_ISDIR(statbuf.st_mode)) {
             if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
                 continue;
+            
             dirwalk(fullpath, options);
         }
 
@@ -37,18 +44,30 @@ void dirwalk(const char *path, const char *options) {
             (strstr(options, "d") != NULL && S_ISDIR(statbuf.st_mode)) ||
             (strstr(options, "f") != NULL && S_ISREG(statbuf.st_mode)) ||
             (strcmp(options, "") == 0 && (S_ISLNK(statbuf.st_mode) || S_ISDIR(statbuf.st_mode) || S_ISREG(statbuf.st_mode)))) {
-            printf("%s\n", fullpath + 2);
+            char *filename = strdup(entry->d_name);
+            files = realloc(files, (files_count + 1) * sizeof(char *));
+            files[files_count++] = filename;
         }
     }
 
     closedir(dir);
+
+    if (strstr(options, "s") != NULL) {
+        qsort(files, files_count, sizeof(char *), compare_strings);
+    }
+
+    for (int i = 0; i < files_count; ++i) {
+        printf("%s/%s\n", path + 2, files[i]);
+        free(files[i]);
+    }
+    free(files);
 }
 
 int main(int argc, char *argv[]) {
-    setlocale(LC_COLLATE, "");
+    //setlocale(LC_COLLATE, "C");
 
     const char *path = (argc > 1) ? argv[1] : ".";
-    char options[10] = ""; // Создаем буфер для опций
+    char options[10] = "";
 
     int opt;
     while ((opt = getopt(argc, argv, "ldfs")) != -1) {
@@ -57,7 +76,7 @@ int main(int argc, char *argv[]) {
             case 'd':
             case 'f':
             case 's':
-                strcat(options, (char[]){opt, '\0'}); // Копируем опцию в буфер
+                strcat(options, (char[]){opt, '\0'});
                 break;
             default:
                 break;
